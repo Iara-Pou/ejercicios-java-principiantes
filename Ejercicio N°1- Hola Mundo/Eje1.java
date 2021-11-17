a650feaca346def7168ed14b15fb9dddbb0fb30f
iara <iarapou21@hotmail.com>
2021-11-17 11:55:25 -0300

    cambios del 17

---
+++ Ejercicio N°1- Hola Mundo/Eje1.java
@@ -0,0 +1,10 @@
+package eje1;
+
+public class Eje1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("¡Hola mundo!");
+    }
+    
+}
---
+++ Ejercicio N°2- Variables/Eje2.java
@@ -0,0 +1,13 @@
+package eje2;
+public class Eje2 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int PrimerNum = 1;
+        int SegundoNum = 2;
+        int TercerNum = 0;
+        
+        TercerNum= PrimerNum + SegundoNum;
+        System.out.println(TercerNum);
+        }
+    
+}
